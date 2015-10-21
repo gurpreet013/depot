439cfded1b601cffe207958685a62fd4eee0686b
Gurpreet <gurpreet@vinsol.com>
2015-10-21 13:12:55 +0530

    uniquesness, presence and oneleveltesting validations is done

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,14 @@ class Category < ActiveRecord::Base
   has_many :products
   belongs_to :parent, class_name: :Category
   has_many :sub_categories, class_name: :Category, foreign_key: :parent_id
+  validates :title, presence: true
+  validates :title, uniqueness:{ scope: [:parent_id, :title] }, if: "!title.nil?"
+  validate :one_level_nesting
+
+  def one_level_nesting
+    if self.class.exists? parent && self.class.exists?(parent.parent)
+      raise 'Sub Category cannot have their own sub category'
+    end
+  end
+
 end
